953d70439306e1c3cec1598a9f382ba023af6900
toueileb <toueilebmohamed@gmail.com>
2020-08-21 13:25:44 +0200

    add react-icons package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FaReact} from 'react-icons/fa'
+import {MdAlarm} from "react-icons/md";
+import {IconContext} from 'react-icons'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <IconContext.Provider value={{color: 'blue', size: '5rem'}}>
+            <div className="App">
+                <FaReact />
+                <MdAlarm />
+            </div>
+        </IconContext.Provider>
+    );
 }
 
 export default App;
